3127e92e6bd913c155021c6a3b9081274b23e251
aysegulyilmaz5 <83874900+aysegulyilmaz5@users.noreply.github.com>
2022-02-07 00:33:58 +0300

    Create num_boolean.dart

---
+++ num_boolean.dart
@@ -0,0 +1,31 @@
+void main(List<String> args){
+    int age = 34;
+  print(age);
+  age = 44;
+  print(age);
+  
+  num year = 1988;
+  print(year);
+  
+  double number = 52;
+  print(number);
+  
+  int weight = 84.6.toInt();
+  
+  var number2 = 56;
+  
+  int numb;
+  numb = 9;
+  print(numb + 9);
+  
+  double? s1 = null;
+  print(s1);
+  
+  /*  double? s1 = null;
+  print(s1 +5); (there is an error) */
+
+  
+  /*int numb;(null safety)
+  print(numb);*/
+  
+}
